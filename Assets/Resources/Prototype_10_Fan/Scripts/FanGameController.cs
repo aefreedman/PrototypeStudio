278f50a9e4b8b85316273c818a77d65c545bdea8
Aaron Freedman <afreedman.soka@gmail.com>
2014-04-16 15:34:04 -0400

    Fangame

---
+++ Assets/Resources/Prototype_10_Fan/Scripts/FanGameController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class FanGameController : MonoBehaviour
+{
+
+    CharacterController controller;
+    
+    private void Start()
+    {
+        Screen.lockCursor = true;
+        Screen.showCursor = false;
+        
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetAxis("Mouse X") != 0)
+        {
+            controller.SimpleMove(transform.forward * 100.0f * Time.deltaTime);
+        }
+    }
+}
